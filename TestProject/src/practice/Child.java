692ea83720081169731cc26052786a5fd6fb6056
Aritra <aritra1704@gmail.com>
2019-11-28 19:27:02 +0530

    Test classes

---
+++ TestProject/src/practice/Child.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Child extends Parent {
+
+	private String childAddress = "";
+	
+	public Child(String address, double lat, double lng) {
+		super(lat, lng);
+		childAddress = address;
+	}
+	
+	public String getAddress() {
+		return childAddress;
+	}
+
+}
